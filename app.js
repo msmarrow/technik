626ba9fed2e3188e90c7049f33e48a37251ad66f
msmarrow <msmarrow@cs.uchicago.edu>
2019-07-21 20:51:57 -0700

    updated config file

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+var port = 8080;
+
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb');
+const routes = require('./routes/index');
+const router = express.Router();
 
 const app = new express();
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '')));
 
+// app.use('/',routes);
+// routes.initalize(app);
+
 app.get('/', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(8080, () => {
-  console.log('App listening on port 4000')
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
+
+module.exports = router;
